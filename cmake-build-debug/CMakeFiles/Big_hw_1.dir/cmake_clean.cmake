eb834737bb46e3f16d3e8785c134d2961c0953a3
JingtaoXu <64328665+JingtaoXu@users.noreply.github.com>
2020-04-26 18:04:36 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Big_hw_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Big_hw_1"
+  "Big_hw_1.pdb"
+  "CMakeFiles/Big_hw_1.dir/Multiplicator.cpp.o"
+  "CMakeFiles/Big_hw_1.dir/Number.cpp.o"
+  "CMakeFiles/Big_hw_1.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Big_hw_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
